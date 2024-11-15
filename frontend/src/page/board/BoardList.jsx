784745d884a234a65b0ffc6c847336f41330f39e
sebaek <sebaek@gmail.com>
2024-11-15 14:26:07 +0900

    검색정보 initial render 시 유지
    키워드만 유지됨...
    검색 타입은 추후 보완...

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -17,9 +17,12 @@ import { Button } from "../../components/ui/button.jsx";
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const [count, setCount] = useState(0);
-  const [search, setSearch] = useState({ type: "all", keyword: "" });
-  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState({
+    type: searchParams.get("st") ?? "all",
+    keyword: searchParams.get("sk") ?? "",
+  });
+  const navigate = useNavigate();
 
   useEffect(() => {
     const controller = new AbortController();
