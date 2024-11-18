23b838bd44e6c2eabe53b9f02d8542fe40946942
sebaek <sebaek@gmail.com>
2024-11-18 14:09:16 +0900

    회원 가입시 이메일 받기

--- sql/member.sql
+++ sql/member.sql
@@ -8,3 +8,12 @@ CREATE TABLE member
 );
 SELECT *
 FROM member;
+
+DROP TABLE member;
+
+ALTER TABLE member
+    ADD COLUMN email VARCHAR(300) UNIQUE AFTER id;
+
+DESC member;
+SELECT *
+FROM member;
