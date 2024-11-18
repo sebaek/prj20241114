47a7fa971b6597d1fc863aefc79f16ea4faabe25
sebaek <sebaek@gmail.com>
2024-11-18 14:30:03 +0900

    이메일 중복 확인

--- frontend/src/page/member/MemberSignup.jsx
+++ frontend/src/page/member/MemberSignup.jsx
@@ -12,7 +12,7 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [description, setDescription] = useState("");
   const [idCheck, setIdCheck] = useState(false);
-  const [emailCheck, setEmailCheck] = useState(false);
+  const [emailCheck, setEmailCheck] = useState(true);
   const [passwordCheck, setPasswordCheck] = useState("");
   const navigate = useNavigate();
 
@@ -84,12 +84,17 @@ export function MemberSignup() {
       });
   };
 
+  // 이메일 중복확인 버튼 활성화 여부
+  let emailCheckButtonDisabled = email.length === 0;
+
   // 가입 버튼 비활성화 여부
   let disabled = true;
 
   if (idCheck) {
-    if (password === passwordCheck) {
-      disabled = false;
+    if (emailCheck) {
+      if (password === passwordCheck) {
+        disabled = false;
+      }
     }
   }
 
@@ -112,9 +117,25 @@ export function MemberSignup() {
           </Group>
         </Field>
         <Field label={"이메일"}>
-          <Group>
-            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-            <Button onClick={handleEmailCheckClick} variant={"outline"}>
+          <Group attached w={"100%"}>
+            <Input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                // 이메일은 필수 입력이 아니어서
+                // 입력하지 않을 겨우 중복체크 하지 않아도됨
+                if (e.target.value.length > 0) {
+                  setEmailCheck(false);
+                } else {
+                  setEmailCheck(true);
+                }
+              }}
+            />
+            <Button
+              disabled={emailCheckButtonDisabled}
+              onClick={handleEmailCheckClick}
+              variant={"outline"}
+            >
               중복확인
             </Button>
           </Group>
