7ee89f563b323c3a91696ff23fd0f22c6ca228d8
sebaek <sebaek@gmail.com>
2024-11-19 14:47:20 +0900

    권한 테이블 생성
    trump에 admin, manager 권한 부여
    
    토큰 생성시 권한정보 (scope) 추가

--- backend/src/main/java/com/example/backend/mapper/member/MemberMapper.java
+++ backend/src/main/java/com/example/backend/mapper/member/MemberMapper.java
@@ -52,4 +52,11 @@ public interface MemberMapper {
             WHERE email = #{email}
             """)
     Member selectByEmail(String email);
+
+    @Select("""
+            SELECT auth
+            FROM auth
+            WHERE member_id = #{id}
+            """)
+    List<String> selectAuthByMemberId(String id);
 }
