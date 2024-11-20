287e89df965f712d25f0ba4f70f549c8eb7b56e2
sebaek <sebaek@gmail.com>
2024-11-20 14:49:24 +0900

    댓글 수정

--- backend/src/main/java/com/example/backend/service/comment/CommentService.java
+++ backend/src/main/java/com/example/backend/service/comment/CommentService.java
@@ -34,4 +34,10 @@ public class CommentService {
     public void remove(Integer id) {
         mapper.deleteById(id);
     }
+
+    public boolean update(Comment comment) {
+        int cnt = mapper.update(comment);
+
+        return cnt == 1;
+    }
 }
